4f4fae76ad452e9b7e8930761ab26711b4dd9ed8
Препод <@4-5>
2024-09-23 21:10:17 +0300

    Добавьте файлы проекта.

---
+++ WpfApp18/Delegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace The_template_is_scanned_MVVM
+{
+    internal class DelegateCommand : ICommand
+    {
+        private Action<object> execute;
+        public DelegateCommand(Action<object> executeAction)
+        {
+            execute = executeAction;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ WpfApp18/ViewModel/UserViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using The_template_is_scanned_MVVM;
+using WpfApp18.Model;
+
+namespace WpfApp18.ViewModel
+{
+    internal class UserViewModel : INotifyPropertyChanged
+    {
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get { return _currentUser; }
+            set
+            {
+                _currentUser = value;
+                OnPropertyChanged("CurrentUser");
+            }
+        }
+
+        private User _tempUser;
+        public User TempUser
+        {
+            get { return _tempUser; }
+            set
+            {
+                _tempUser = value;
+                OnPropertyChanged("TempUser");
+            }
+        }
+
+        public ICommand AddUser { get; set; }
+
+        public UserViewModel()
+        {
+            CurrentUser = new User();
+            TempUser = new User(); 
+            AddUser = new DelegateCommand(AddUserHandler);
+        }
+
+        public void AddUserHandler(object obj)
+        {
+            CurrentUser = new User()
+            {
+                Email = TempUser.Email,
+                Password = TempUser.Password,
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string param)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(param));
+        }
+    }
+}
